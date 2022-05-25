c4b8e592ac25fd8eeba7af0e76064c970120768d
Ivens <ivensarroxellas@hotmail.com>
2022-05-25 17:47:24 -0300

    socorro

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-window.onload{
-    document.getElementById("")
+let linha = 5;
+let coluna = 5;
+
+for (let indexL = 0; indexL < linha; indexL += 1) {
+    let linhaTabela = document.createElement("div")
+    document.getElementById("pixel-board").appendChild(linhaTabela)
+    linhaTabela.classList.add("linhaTabela")
+    for (let indexC = 0; indexC < coluna; indexC += 1) {
+        let colunaTabela = document.createElement("div")
+        linhaTabela.appendChild(colunaTabela)
+        colunaTabela.classList.add("pixel")
+    }
+    
 }
